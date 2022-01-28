2c6d313f35bb2cc8ac34477b295a48e78186e4bc
jordanbsandoval <1608@holbertonschool.com>
2022-01-27 23:14:57 -0400

    try-except-finally3

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,36 +1,31 @@
 #!/usr/bin/python3
-"""0x06. Log Parsing"""
-from sys import stdin
+""" Parses Logs """
+import sys
 
 
-total_size = 0
-stats = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
-         '404': 0, '405': 0, '500': 0}
-
-
-def print_stats():
-    """A script that reads stdin line by line and computes metrics"""
-    print("File size: {}".format(total_size))
-    for key, value in sorted(stats.items()):
-        if value > 0:
-            print("{}: {}".format(key, value))
-
-
-if __name__ == "__main__":
-    line_counter = 0
-    try:
-        for line in stdin:
-            args = line.split()
-            line_counter += 1
-            if len(args) > 2:
-                total_size += int(args[-1])
-                if args[-2] in stats:
-                    stats.update({str(args[-2]): stats.get(args[-2]) + 1})
-            if line_counter == 10:
-                print_stats()
-                line_counter = 0
-    except KeyboardInterrupt:
-        print_stats()
-        raise
-    print_stats()
+i = 0
+FileSize = 0
+status = {'200': 0, '301': 0, '400': 0, '401': 0,
+          '403': 0, '404': 0, '405': 0, '500': 0}
+codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+try:
+    for line in sys.stdin:
+        i += 1
+        sp = line.split(' ')
+        if len(sp) > 2:
+            FileSize += int(sp[-1])
+            if sp[-2] in status:
+                status[sp[-2]] += 1
+        if i % 10 == 0:
+            print("File size: {}".format(FileSize))
+            for code in codes:
+                if status[code]:
+                    print("{}: {}".format(code, status[code]))
+except KeyboardInterrupt:
+    pass
+finally:
+    print("File size: {}".format(FileSize))
+    for code in codes:
+        if status[code]:
+            print("{}: {}".format(code, status[code])
 
