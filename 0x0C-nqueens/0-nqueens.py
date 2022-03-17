fdb6d138f6f851ea53a5ec6194cbc3897f039414
jordanbsandoval <1608@holbertonschool.com>
2022-03-17 19:46:00 -0400

    queens

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -23,6 +23,7 @@ def Fill_line(Board, line):
             else:
                 print([[i, Board[i]] for i in range(len(Board))])
 
+
 if len(sys.argv) != 2:
     print("Usage: nqueens N")
     sys.exit(1)
@@ -31,6 +32,7 @@ try:
 except:
     print("N must be a number")
     sys.exit(1)
+
 if n < 4:
     print("N must be at least 4")
     sys.exit(1)
